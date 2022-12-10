f0a9eac98685fd323b19529292c36456d5923962
Jakub Miazek <the@grillazz.com>
2022-12-10 10:59:18 +0100

    api router wip

--- fastapi_apscheduler/routers.py
+++ fastapi_apscheduler/routers.py
@@ -1,4 +1,12 @@
+import pickle
+
 from fastapi import APIRouter, Request, status
+from .utils import get_logger
+
+logger = get_logger(__name__)
+
+class JobNotFound(Exception):
+    pass
 
 
 def get_users_router() -> APIRouter:
@@ -13,10 +21,21 @@ def get_users_router() -> APIRouter:
     @router.get("", name="scheduler:get_jobs", response_model=list)
     async def get_jobs(request: Request):
         jobs = request.app.state.scheduler.get_jobs()
-        jobs = [
-            dict((k, v) for k, v in job.__getstate__().items() if k != "trigger")
-            for job in jobs
-        ]
-        return jobs
+        # jobs = [
+        #     dict((k, v) for k, v in job.__getstate__().items() if k != "trigger")
+        #     for job in jobs
+        # ]
+        jobs = [pickle.dumps(job.__getstate__(), 5) for job in jobs]
+        logger.debug(jobs)
+        # return jobs
+
+    @router.delete("/{job_id}", name="scheduler:remove_job")
+    async def remove_job(request: Request, job_id: str):
+        try:
+            deleted = request.app.state.scheduler.remove_job(job_id=job_id)
+            logger.debug(f"Job {job_id} deleted: {deleted}")
+            return {"job": f"{job_id}"}
+        except AttributeError:
+            raise JobNotFound(f"No job by the id of {job_id} was found")
 
     return router
