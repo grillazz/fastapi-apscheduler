e54aa2d8aa1be91ca34eaf623342459b747a929e
Jakub Miazek <the@grillazz.com>
2023-12-12 21:18:06 +0100

    adapt pydantic v2

--- fastapi_apscheduler/schemas.py
+++ fastapi_apscheduler/schemas.py
@@ -1,5 +1,9 @@
 from enum import Enum
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
+
+config = ConfigDict(json_schema_extra={
+    "example": {"func": "example.main:pytest_job", "trigger": "interval", "seconds": 3, "id": "pytest_job"}
+})
 
 
 class TriggerEnum(str, Enum):
@@ -8,12 +12,8 @@ class TriggerEnum(str, Enum):
 
 
 class Job(BaseModel):
+    model_config = config
     func: str = Field()  # TODO: PyObject https://github.com/pydantic/pydantic/issues/4079
-    trigger: TriggerEnum = Field(..., title="Trigger type")
-    seconds: int = Field(..., title="Interval in seconds")
-    id: str = Field(..., title="Job ID")
-
-    class Config:
-        schema_extra = {
-            "example": {"func": "example.main:pytest_job", "trigger": "interval", "seconds": 3, "id": "pytest_job"}
-        }
+    trigger: TriggerEnum = Field(title="Trigger type")
+    seconds: int = Field(title="Interval in seconds")
+    id: str = Field(title="Job ID")
