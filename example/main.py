d0067e19a794f47b9dd74cf931404a0a76603af7
Jakub Miazek <the@grillazz.com>
2023-12-14 11:35:14 +0100

    adding pottery

--- example/main.py
+++ example/main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from fastapi import FastAPI
 
 from fastapi_apscheduler.utils import get_logger
@@ -12,9 +14,10 @@ app.include_router(get_jobs_router(), prefix="/scheduler", tags=["scheduler"])
 
 
 async def pytest_job():
-    logger.info("test_job")
+    logger.info("dupa_test_job")
 
-# TODO: job to test netowrk seed with ookla pytjhon client via apscheduler
+
+# TODO: job to test netowrk seed with ookla python client via apscheduler
 # TODO: mount via lifespan should happen as meny time as many workers you add
 # TODO: fix duppl;icates via redis aio locks
 
--- fastapi_apscheduler/scheduler.py
+++ fastapi_apscheduler/scheduler.py
@@ -1,5 +1,5 @@
 from contextlib import asynccontextmanager
-
+import os
 from fastapi import FastAPI
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -7,13 +7,35 @@ from .utils import get_logger
 
 logger = get_logger(__name__)
 
+import redis.asyncio as redis
+from pottery import AIORedLock
+
+async def get_redis():
+    return await redis.from_url(
+        "redis://localhost:6379/2",
+        encoding="utf-8",
+        decode_responses=True,
+    )
+
+async def create_scheduler(app, db_url="sqlite:///jobs1.sqlite"):
+    _pid = os.getpid()
+    logger.info(f"create_scheduler pid: {_pid}")
+    jobstores = {"default": SQLAlchemyJobStore(url=db_url)}
+    app.state.scheduler = AsyncIOScheduler(jobstores=jobstores)
+
+    app.state.scheduler.start()
+
+
+def stop_scheduler(app):
+    app.state.scheduler.shutdown()
+
+
+    # app.state.scheduler.shutdown()
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    jobstores = {"default": SQLAlchemyJobStore(url="sqlite:///jobs.sqlite")}
-    app.state.scheduler = AsyncIOScheduler(jobstores=jobstores)  # TODO: set this as variable via BaseConfig class etc
-    app.state.scheduler.start()
-    logger.info(f"startup event - {app.state.scheduler.state=}")
-    try:
-        yield
-    finally:
-        app.state.scheduler.shutdown()
+    await create_scheduler(app)
+    yield
+    stop_scheduler(app)
+
+
