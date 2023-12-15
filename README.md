e46f9055d7fd9cb47026a8c545a06e2fd5eca7df
Jakub Miazek <the@grillazz.com>
2023-12-15 13:56:48 +0100

    fix test

--- README.md
+++ README.md
@@ -17,15 +17,12 @@ pip install fastapi-apscheduler
 from fastapi import FastAPI
 
 from fastapi_apscheduler.utils import get_logger
-from fastapi_apscheduler.scheduler import add_scheduler
 from fastapi_apscheduler.routers import get_jobs_router
+from fastapi_apscheduler.scheduler import lifespan
 
 logger = get_logger(__name__)
 
-app = FastAPI()
-
-
-add_scheduler(app, "sqlite:///jobs.sqlite")
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(get_jobs_router(), prefix="/scheduler", tags=["scheduler"])
 
--- example/main.py
+++ example/main.py
@@ -1,12 +1,8 @@
-import asyncio
-
 from fastapi import FastAPI
 
 from fastapi_apscheduler.utils import get_logger
 from fastapi_apscheduler.routers import get_jobs_router
 from fastapi_apscheduler.scheduler import lifespan
-
-import redis.asyncio as redis
 
 logger = get_logger(__name__)
 
@@ -15,18 +11,8 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(get_jobs_router(), prefix="/scheduler", tags=["scheduler"])
 
 
-async def get_redis():
-    return await redis.from_url(
-        "redis://localhost:6379/2",
-        encoding="utf-8",
-        decode_responses=True,
-    )
-
 async def pytest_job():
-    logger.info("dupa_test_job")
-
-# async def pytest_job():
-#     logger.info("dupa_test_job")
+    logger.info("test_job")
 
 
 
