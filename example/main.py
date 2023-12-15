f4744611fe53d7037a1d1af02b4f2b17854a3a29
Jakub Miazek <the@grillazz.com>
2023-12-15 13:39:28 +0100

    mount scheduler via lifespan

--- example/main.py
+++ example/main.py
@@ -6,6 +6,8 @@ from fastapi_apscheduler.utils import get_logger
 from fastapi_apscheduler.routers import get_jobs_router
 from fastapi_apscheduler.scheduler import lifespan
 
+import redis.asyncio as redis
+
 logger = get_logger(__name__)
 
 app = FastAPI(lifespan=lifespan)
@@ -13,15 +15,18 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(get_jobs_router(), prefix="/scheduler", tags=["scheduler"])
 
 
+async def get_redis():
+    return await redis.from_url(
+        "redis://localhost:6379/2",
+        encoding="utf-8",
+        decode_responses=True,
+    )
+
 async def pytest_job():
     logger.info("dupa_test_job")
 
-
-# TODO: job to test netowrk seed with ookla python client via apscheduler
-# TODO: mount via lifespan should happen as meny time as many workers you add
-# TODO: fix duppl;icates via redis aio locks
+# async def pytest_job():
+#     logger.info("dupa_test_job")
 
 
-# TODO: test with gunicorn uvicorn workers
-# TODO: gunicorn example.main:app -w 5 -k uvicorn.workers.UvicornWorker
-# TODO: add some gui for example with i.e. https://github.com/pydantic/FastUI
+
--- fastapi_apscheduler/scheduler.py
+++ fastapi_apscheduler/scheduler.py
@@ -1,41 +1,20 @@
 from contextlib import asynccontextmanager
-import os
+
 from fastapi import FastAPI
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 from .utils import get_logger
 
 logger = get_logger(__name__)
 
-import redis.asyncio as redis
-from pottery import AIORedLock
-
-async def get_redis():
-    return await redis.from_url(
-        "redis://localhost:6379/2",
-        encoding="utf-8",
-        decode_responses=True,
-    )
-
-async def create_scheduler(app, db_url="sqlite:///jobs1.sqlite"):
-    _pid = os.getpid()
-    logger.info(f"create_scheduler pid: {_pid}")
-    jobstores = {"default": SQLAlchemyJobStore(url=db_url)}
-    app.state.scheduler = AsyncIOScheduler(jobstores=jobstores)
-
-    app.state.scheduler.start()
-
-
-def stop_scheduler(app):
-    app.state.scheduler.shutdown()
-
-
-    # app.state.scheduler.shutdown()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await create_scheduler(app)
-    yield
-    stop_scheduler(app)
-
-
+    jobstores = {"default": SQLAlchemyJobStore(url="sqlite:///jobs1.sqlite")}
+    app.state.scheduler = AsyncIOScheduler(jobstores=jobstores)
+    try:
+        app.state.scheduler.start()
+        yield
+    finally:
+        app.state.scheduler.shutdown()
