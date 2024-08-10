990e335f0abb03451df6a4c36dd4f27ef1954f43
grillazz <the@grillazz.com>
2024-08-10 15:39:27 +0200

    wip: job schema change proposal

--- fastapi_apscheduler/schemas.py
+++ fastapi_apscheduler/schemas.py
@@ -19,3 +19,11 @@ class Job(BaseModel):
     trigger: TriggerEnum = Field(title="Trigger type")
     seconds: int = Field(title="Interval in seconds")
     id: str = Field(title="Job ID")
+
+# TODO: Add this to the Job model to warrant unique IDs
+# from pydantic import Field
+# from uuid import uuid4
+#
+# class Job(BaseModel):
+#     id: str = Field(default_factory=lambda: str(uuid4()), title="Job ID")
+#     # other fields...
